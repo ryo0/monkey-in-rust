687fa90988155650fdcad089ab83db5de387614d
ryo0 <nekonekosekai@gmail.com>
2020-08-13 12:23:03 +0900

    tokenizerをパターンマッチ使うように修正中

--- src/main.rs
+++ src/main.rs
@@ -79,6 +79,7 @@ fn main() {
         String::from("else"),
         String::from("return"),
     ];
+
     let mut reserved_word_tokens = vec![
         Token::Let,
         Token::Fn,
@@ -93,8 +94,7 @@ fn main() {
     reserved_word_tokens.append(&mut symbol_tokens);
 
     let tokens_map: HashMap<_, _> = words.iter().zip(reserved_word_tokens.iter()).collect();
-    let input = String::from(
-        "
+    let input = "
 let add = fn(x, y) {
     let a = x + y - z < w * a / b != k == d;
 };
@@ -102,30 +102,12 @@ if (5 < 10) {
     return true;
 } else {
     return false;
-}",
-    );
-    print!("{:?}", tokenize(input, tokens_map));
+}";
+    print!("{:?}", tokenize(split_string(input).as_slice(), tokens_map));
 }
 
-fn get_char(str: &String, position: i32) -> String {
-    match str.chars().nth(position as usize) {
-        Some(x) => x.to_string(),
-        None => String::from(""),
-    }
-}
-
-fn is_letter(s: &str) -> bool {
-    if let Some(c) = s.chars().nth(0) {
-        if c.is_alphabetic() {
-            true
-        } else if c == '_' {
-            true
-        } else {
-            false
-        }
-    } else {
-        false
-    }
+fn split_string(s: &str) -> Vec<char> {
+    s.chars().collect()
 }
 
 fn is_num(s: &str) -> bool {
@@ -140,15 +122,12 @@ fn is_num(s: &str) -> bool {
     }
 }
 
-fn get_letter(str: &String, position: i32) -> (String, i32) {
-    let mut p = position;
-    loop {
-        let c = get_char(str, p);
-        if is_letter(&c) {
-            p += 1;
-        } else {
-            return (str[position as usize..p as usize].to_string(), p);
+fn get_letter(s: &[char], acm: String) -> (String, &[char]) {
+    match s {
+        [first, rest @ ..] if first.is_alphabetic() || first == &'_' => {
+            get_letter(rest, format!("{}{}", acm, first))
         }
+        _ => (acm, s),
     }
 }
 
@@ -164,53 +143,24 @@ fn get_num(str: &String, position: i32) -> (String, i32) {
     }
 }
 
-fn tokenize(str: String, token_map: HashMap<&String, &Token>) -> Vec<Token> {
-    let mut p = 0;
-    let mut tokens: Vec<Token> = vec![];
-    loop {
-        let c = get_char(&str, p);
-        if p >= str.len() as i32 {
-            return tokens;
+fn tokenize<'a, 'b>(
+    s: &'a [char],
+    tokens: &Vec<&Token>,
+    token_map: HashMap<&String, &Token>,
+) -> (&'a [char], &'b Vec<Token>) {
+    match s {
+        [first, rest @ ..] if first.is_alphabetic() => {
+            let (letter, rest) = get_letter(s, String::from(""));
+             let token = match token_map.get(&letter) {
+                 Some(token) =>{ *token }
+                 None =>{ panic!("token_map") }
+             }
+            tokens.push(token);
+            tokenize(rest, tokens, token_map)
         }
-        if c.is_empty() {
-            return tokens;
-        }
-        if c == String::from("\n") {
-            p += 1
-        } else if c == String::from(" ") || c == String::from("    ") {
-            p += 1
-        } else if is_letter(&c) {
-            let (letter, pss) = get_letter(&str, p);
-            if let Some(&reserved_token) = token_map.get(&letter) {
-                tokens.push(reserved_token.clone())
-            } else {
-                tokens.push(Token::Var(letter));
-            }
-            p = pss;
-        } else if is_num(&c) {
-            let (n, pos) = get_num(&str, p);
-            p = pos;
-            let n: i32 = n.parse().unwrap();
-            tokens.push(Token::Int(n));
-        } else {
-            let next_c = if p + 1 < str.len() as i32 {
-                Some(get_char(&str, p + 1))
-            } else {
-                None
-            };
-            if c == String::from("=") && next_c == Some(String::from("=")) {
-                tokens.push(Token::Equal);
-                p += 2;
-            } else if c == String::from("!") && next_c == Some(String::from("=")) {
-                tokens.push(Token::NotEqual);
-                p += 2;
-            } else {
-                if let Some(&t) = token_map.get(&c) {
-                    tokens.push(t.clone());
-                    p += 1;
-                    continue;
-                }
-            }
+
+        [first, rest @ ..] if first.is_numeric() => {
+            
         }
     }
 }
---
+++ target/debug/libmonkey.d
@@ -0,0 +1,1 @@
+/home/ryo/ryo0/monkey/target/debug/libmonkey.rlib: /home/ryo/ryo0/monkey/src/lib.rs
